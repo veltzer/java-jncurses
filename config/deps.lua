856bdedc7851ce0eeed2b280fc946276c4c4dc9a
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-21 12:52:09 +0300

    no commit message given

---
+++ config/deps.lua
@@ -0,0 +1,14 @@
+-- os level dependencies for this project
+
+PACKAGES = {
+    -- for creating the interfaces
+    "swig",
+    -- for the examples
+    "swig-examples",
+    -- for the documentation
+    "swig-doc",
+    -- for development
+    "gcc-doc",
+    -- for swig file
+    "autoconf-archive",
+}
